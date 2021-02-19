0c09f567fe793adadf38c7999615029231006bb1
Jaideep Chauhan <67269475+Jaideep25@users.noreply.github.com>
2021-02-19 11:12:04 +0530

    Update monkey virus.bat

--- monkey virus.bat
+++ monkey virus.bat
@@ -1,17 +1,5 @@
-@echo off
+@echo on
 color 57
-echo Hey, is Jaideep Cool ? [ANSWER IN yes OR no]
-set /p love=
-if %love%==yes goto cool
-if %love%==no goto hate
-:cool
-echo You are right Jaideep Is cool!!!...
-echo Meet you soon :)
-pause 
-exit
-:hate
-echo But Jaideep Is cool !!!....hehehehehe
-echo You are hacked...
 echo Your PC will crash in 10 seconds
 timeout 10
 shutdown -s -t 100
